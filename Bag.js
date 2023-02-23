ae59580c9c810808870815a7b9b5c33b32c3283e
colledwards <colledwards@expediagroup.com>
2023-02-23 13:04:55 -0800

    added static maxWeight value and improved tests

--- Bag.js
+++ Bag.js
@@ -1,8 +1,8 @@
 class Bag {
+    static maxWeight = 23;
     constructor(weight) {
-        if(!weight){
-            throw new Error('bag needs weight')
-        } 
+        if(!weight) throw new Error('bag needs weight');
+        if(weight > Bag.maxWeight) throw new Error('weight exceeds maximum');
         this.weight = weight
     }
 }
--- Bag.test.js
+++ Bag.test.js
@@ -3,11 +3,14 @@ const Bag = require('./Bag')
 describe('Bag class', () => {
     //bag has weight
     test('bag has weight', () => {
-        const bag = new Bag(25)
-        expect(bag.weight).toBe(25)
+        const bag = new Bag(20)
+        expect(bag.weight).toBe(20)
     })
     //if bag has no weight, return error
     test('bag has no weight to return an error', () => {
         expect(() => new Bag()).toThrowError('bag needs weight')
+    })
+    test('bag exceeds max weight', () => {
+        expect(() => new Bag(25)).toThrow();
     })
 })
